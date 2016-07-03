07bd6b5af73931bc35a5fac65e1890e4d7b11ba5
ぶっちゃん！ <g.dark.matter@gmail.com>
2016-07-03 20:33:42 +0900

    First Commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyVersion("2.6.7.2")]
+[assembly: AssemblyCompany("JO3QMA")]
+[assembly: AssemblyCopyright("Copyright ©2016")]
+[assembly: AssemblyDescription("A program to modify/tweak settings/solve errors for PSO2!")]
+[assembly: AssemblyFileVersion("2.6.7.2")]
+[assembly: AssemblyProduct("PSO2 Tweaker")]
+[assembly: AssemblyTitle("PSO2 Tweaker")]
+[assembly: AssemblyTrademark(null)]
